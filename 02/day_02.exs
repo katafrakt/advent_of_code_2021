f65030bd9d30a204cbcc9a2df2d88a070f904982
Paweł Świątkowski <katafrakt@vivaldi.net>
2021-12-02 20:51:13 +0100

    Day 2, part 2

--- 02/day_02.exs
+++ 02/day_02.exs
@@ -1,2 +1,4 @@
-{h, depth} = SubmarineLang.eval_file("input")
+{h, depth} = SubmarineLang.Interpreter0201.eval_file("input")
+IO.puts(h * depth)
+{h, depth, _aim} = SubmarineLang.Interpreter0202.eval_file("input")
 IO.puts(h * depth)
--- 02/lib/submarine_lang.ex
+++ 02/lib/submarine_lang.ex
@@ -1,4 +1,4 @@
-defmodule SubmarineLang do
+defmodule SubmarineLang.Interpreter0201 do
   def eval_file(name) do
     input =
       name
@@ -14,4 +14,22 @@ defmodule SubmarineLang do
   defp eval({{:move, :forward}, {:number, x}}, {h, depth}), do: {h + x, depth}
   defp eval({{:move, :down}, {:number, x}}, {h, depth}), do: {h, depth + x}
   defp eval({{:move, :up}, {:number, x}}, {h, depth}), do: {h, depth - x}
+end
+
+defmodule SubmarineLang.Interpreter0202 do
+  def eval_file(name) do
+    input =
+      name
+      |> File.read!()
+      |> to_charlist()
+
+    {:ok, tokens, _} = :lexer.string(input)
+    {:ok, ast} = :parser.parse(tokens)
+    eval(ast)
+  end
+
+  defp eval(ast) when is_list(ast), do: Enum.reduce(ast, {0, 0, 0}, &eval/2)
+  defp eval({{:move, :forward}, {:number, x}}, {h, depth, aim}), do: {h + x, depth + (aim * x), aim}
+  defp eval({{:move, :down}, {:number, x}}, {h, depth, aim}), do: {h, depth, aim + x}
+  defp eval({{:move, :up}, {:number, x}}, {h, depth, aim}), do: {h, depth, aim - x}
 end
